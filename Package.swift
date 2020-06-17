9d6bdc1152c910e50022215b1c8373e4b3e7e342
Eimantas Vaičiūnas <eimantas@stork.software>
2020-06-17 15:31:07 +0300

    Match indentation

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@
 import PackageDescription
 
 let package = Package(
-	name: "SavannaKit",
-    platforms: [
-        .macOS(.v10_14)
-    ],
-    products: [
-        .library(name: "SavannaKit", type: .dynamic, targets: ["SavannaKit"])
-    ],
-    targets: [
-        .target(name: "SavannaKit")
-    ]
+  name: "SavannaKit",
+  platforms: [
+    .macOS(.v10_14)
+  ],
+  products: [
+    .library(name: "SavannaKit", type: .dynamic, targets: ["SavannaKit"])
+  ],
+  targets: [
+    .target(name: "SavannaKit")
+  ]
 )
