c5f2119c95a2be3dfcbdf7320a90ff7e3be7a7f0
Eimantas Vaičiūnas <eimantas@stork.software>
2020-06-17 15:14:15 +0300

    Reduce macOS deployment version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
 	name: "SavannaKit",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_14)
     ],
     products: [
         .library(name: "SavannaKit", type: .dynamic, targets: ["SavannaKit"])
